3e51a8cf6587d5aca06bc835341ad64517f80231
russell258 <duckie258@gmail.com>
2023-05-12 09:26:12 +0800

    first commit for task01 and task02 directory

---
+++ src/main/java/sg/nus/edu/iss/task01/Main.java
@@ -0,0 +1,10 @@
+package sg.nus.edu.iss.task01;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+  
+    }
+}
---
+++ src/main/java/sg/nus/edu/iss/task02/Main.java
@@ -0,0 +1,8 @@
+package sg.nus.edu.iss.task02;
+
+public class Main {
+    public static void main( String[] args )
+    {
+
+    }
+}
