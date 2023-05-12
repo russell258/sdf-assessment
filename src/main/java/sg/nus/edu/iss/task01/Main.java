a1fb383ba360d08d15941624ff19d77e543881c1
russell258 <duckie258@gmail.com>
2023-05-12 09:41:11 +0800

    calculator class with basic methods and scanner

--- src/main/java/sg/nus/edu/iss/task01/Main.java
+++ src/main/java/sg/nus/edu/iss/task01/Main.java
@@ -1,10 +1,38 @@
 package sg.nus.edu.iss.task01;
 
+import java.util.Scanner;
 
-public class App 
+//calculator accepts line input separated by single space and calculates based on number and "operator"/"symbol" given
+
+        // Number and operators are separated by a single space. Else....?
+        // 2. create function for each operator
+        // all returned values should be assigned to $last
+        // Add
+        // Subtract 
+        // Divide
+        // Multiply
+        // Number can be positive, negative, INTEGER or Decimal
+
+public class Main 
 {
     public static void main( String[] args )
     {
-  
+        String commandInput="";
+
+
+        //Welcome message
+        System.out.println("Welcome.");
+
+        //scanner to take system.input
+        Scanner scan = new Scanner(System.in);
+
+        // while input is not exit
+        while (!commandInput.equals("exit")){
+            commandInput = scan.nextLine();
+        }
+        
+
+
+        scan.close();
     }
 }
