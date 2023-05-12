98e9b32b414faf1038f14041c8c889035b10a8b8
russell258 <duckie258@gmail.com>
2023-05-12 11:59:08 +0800

    comments for task02

--- src/main/java/sg/nus/edu/iss/task02/Main.java
+++ src/main/java/sg/nus/edu/iss/task02/Main.java
@@ -3,6 +3,11 @@ package sg.nus.edu.iss.task02;
 public class Main {
     public static void main( String[] args )
     {
-        
+        /*add directory texts inside task02
+         - program has to run with argument so check arg length
+         - check directory exists
+         - FileReader >> bufferedReader >> readlines >> split and replace punctation, lowercase all
+         - create hashmap
+         */
     }
 }
